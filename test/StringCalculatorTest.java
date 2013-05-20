e6889f4ecebad7c15d29e76806b5f1dc9cc369bf
Luis <luisrovirosa@gmail.com>
2013-05-20 11:00:17 +0200

    Refactoring

--- test/StringCalculatorTest.java
+++ test/StringCalculatorTest.java
@@ -5,15 +5,15 @@ import org.junit.Test;
 
 public class StringCalculatorTest {
 
+	final StringCalculator calc = new StringCalculator();
+	
 	@Test
 	public void returnsZeroWhenEmptyString() throws Exception {
-		StringCalculator calc = new StringCalculator();
 		assertEquals(0, calc.add(""));
 	}
 
 	@Test
 	public void returnsNumberWhenSingleString() throws Exception {
-		StringCalculator calc = new StringCalculator();
 		assertEquals(1, calc.add("1"));
 	}
 
