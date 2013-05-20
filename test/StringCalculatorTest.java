8ba4c9ffe980f2f75be378a26d62ab67a7d3c355
Luis <luisrovirosa@gmail.com>
2013-05-20 11:25:30 +0200

    Mas pruebas

--- test/StringCalculatorTest.java
+++ test/StringCalculatorTest.java
@@ -1,12 +1,11 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-
 
 public class StringCalculatorTest {
 
 	final StringCalculator calc = new StringCalculator();
-	
+
 	@Test
 	public void returnsZeroWhenEmptyString() throws Exception {
 		assertEquals(0, calc.add(""));
@@ -15,6 +14,9 @@ public class StringCalculatorTest {
 	@Test
 	public void returnsNumberWhenSingleString() throws Exception {
 		assertEquals(1, calc.add("1"));
+		assertEquals(345, calc.add("345"));
+		assertEquals(0, calc.add("0"));
+		assertEquals(-1, calc.add("-1"));
 	}
 
 }
